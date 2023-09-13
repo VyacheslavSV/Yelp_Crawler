8a257d2be55616c9229e45f04c61c0c8aaa02423
@foxstudent102022 <vyacheslavseregin21@gmail.com>
2023-09-13 13:51:56 +0300

    reviews an option that should work but doesn't

--- yelp/yelp/spiders/yelp_spider.py
+++ yelp/yelp/spiders/yelp_spider.py
@@ -6,7 +6,7 @@ CATEGORY = "contractors"
 LOCATION = "San Francisco, CA"
 RANGE = 10
 
-
+# start crawl: input in terminal: scrapy crawl yelp_spider -o output.json
 class YelpSpider(scrapy.Spider):
     name = 'yelp_spider'
 
@@ -15,7 +15,6 @@ class YelpSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-
         for link in response.css('span.css-1egxyvc a::attr(href)'):
             yield response.follow(link, callback=self.parse_results)
 
@@ -25,10 +24,22 @@ class YelpSpider(scrapy.Spider):
 
     def parse_results(self, response):
         business_data = {'business_name': response.css('h1.css-1se8maq::text').get(),
-            'business_rating': response.css('span.css-1p9ibgf::text').get(),
-            'num_reviews': response.css('span.css-1evauet a::text').re(r'(\d+) reviews')[0],
-            'business_yelp_url': response.css('link[rel=canonical]::attr(href)').get(),
-            'business_website': unquote(response.css('p.css-1p9ibgf a::attr(href)').re_first(r'(?<=url=)(.*?)(?=&c)')),
-            'reviews': []}
+                         'business_rating': response.css('span.css-1p9ibgf::text').get(),
+                         'num_reviews': response.css('span.css-1evauet a::text').re(r'(\d+) reviews')[0],
+                         'business_yelp_url': response.css('link[rel=canonical]::attr(href)').get(),
+                         'business_website': unquote(
+                             response.css('p.css-1p9ibgf a::attr(href)').re_first(r'(?<=url=)(.*?)(?=&c)')),
+                         'reviews': []}
+        # I need more time to read the information from the review.
+        # This code should give the necessary data but does not return.
+        for review in response.css('div#reviews ul.list__09f24__ynIEd > li')[:5]:
+            reviewer_name = review.css('span.fs-block.css-ux5mu6 a::text').get()
+            reviewer_location = review.css('div.border-color--default__09f24__NPAKY span::text').get()
+            review_date = review.css(
+                'div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY '
+                'span::text').get()
+
+            business_data['reviews'].append(
+                {'reviewer_name': reviewer_name, 'reviewer_location': reviewer_location, 'review_date': review_date})
 
         yield business_data
